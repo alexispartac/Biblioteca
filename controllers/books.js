8361de6b8269452b297936f1980ea5a01dcb1394
Partac Alexis Matei <alexismateipartac@gmail.com>
2024-06-21 18:34:11 +0300

    final delete

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,7 @@
 
 import mongodb from "mongodb"
 import { MongoClient } from "mongodb";
+import Book from "../model/books.js"
 
 const uri = "mongodb+srv://mateipartac45:Lucaaliuta13$@cluster0.stmiw0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
