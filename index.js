09bc1ad94e76ce775595145707bf66fa62fd1f8d
Partac Alexis Matei <alexismateipartac@gmail.com>
2024-06-22 08:59:30 +0300

    change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+
 //REST API 
 import http from 'http'
 import express from 'express';                          // importam modulul express  
 import bodyParser from 'body-parser';                   //  importam modulul body-parser pentru a putea lucra cu req
 import persoaneRoutes from './routes/persoane.js';
 import cartiRoutes from './routes/carti.js';
-
+import connectDB from "./config/dbConn.js"
+import mongoose from 'mongoose';
 // app
 const app = express();
 app.use(bodyParser.json());
@@ -13,17 +15,22 @@ app.use('/persoane', persoaneRoutes);
 app.use('/carti', cartiRoutes);
 
 const PORT = process.env.PORT || 8080;
+const uri = "mongodb+srv://mateipartac45:Lucaaliuta13$@cluster0.stmiw0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+connectDB();
 
 // Create server 
 const server = http.createServer(app);
-
-server.listen( PORT , (err) => {
-    if (err) {
-        console.log("Not connected to server: ", err);
-    }
-    console.log(`Server running on port: http://localhost:${PORT}`)
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    // Connect to server
+    server.listen( PORT , (err) => {
+        if (err) {
+            console.log("Not connected to server: ", err);
+        }
+        console.log(`Server running on port: http://localhost:${PORT}`)
+    })
 })
-
-
 
 
